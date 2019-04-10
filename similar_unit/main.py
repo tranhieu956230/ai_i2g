70fc9b92d9ee5c61bbba2a06b3d1fae9aecf3233
Hieu <tranhieu956230@gmail.com>
2019-04-10 17:26:54 +0700

    refactor

--- similar_unit/main.py
+++ similar_unit/main.py
@@ -1,61 +1,12 @@
 from csv import DictReader, DictWriter, reader
-
+from utilites import utils_func
 
 def find_unit_core_depofacies(unit_index, data_list):
     depofacies = []
     for row in data_list:
         if row["Unit_index"] in unit_index:
             depofacies.append(row["Core_depofacies"])
-    return removeDuplicate(depofacies)
-
-
-def convert_string_to_array(string):
-    return string[1:-1].split(", ") if string != '[]' else []
-
-
-def map_core_depofacies_code_to_name(code):
-    try:
-        code_to_name = {
-            "1.1": "Alluvial_Fan",
-            "1.2": "Fluvial_Channel",
-            "1.3": "Fluvial_Point_Bar",
-            "1.4": "Levee",
-            "1.5": "Crevasse_Splay",
-            "1.6": "Fluvial_Floodplain",
-            "2.1": "Progradational_Lacustrine_Delta",
-            "2.2": "Lacustrine_Fan_Delta",
-            "2.3": "Progradational_Lacustrine_Shoreface",
-            "2.4": "Transgressive_Lacustrine_Shoreface",
-            "2.5": "Aggradational_Lacustrine_Shoreface",
-            "2.6": "Lacustrine_Offshore_Transition",
-            "2.7": "Lacustrine_Shelf",
-            "3.1": "Proximal_Sub-Lacustrine_Fan",
-            "3.2": "Distal_Sub-Lacustrine_Fan",
-            "3.3": "Lacustrine_Turbidite",
-            "3.4": "Distal_Lacustrine_Turbidites",
-            "3.5": "Lacustrine_Deepwater",
-            "4.1": "Marine_Delta",
-            "4.2": "Marine_Fan_Delta",
-            "4.3": "Tidal_Channel_and_Sand_Flat",
-            "4.4": "Sandy_Tidal_Flat",
-            "4.5": "Mixed_Tidal_Flat",
-            "4.6": "Muddy_Tidal_Flat",
-            "4.7": "Lagoon",
-            "5.1": "Progradational_Marine_Shoreface",
-            "5.2": "Transgressive_Marine_Shoreface",
-            "5.3": "Aggradational_Marine_Shoreface",
-            "5.4": "Marine_Offshore_Transition",
-            "5.5": "Marine_Shelf",
-            "6.1": "Proximal_Submarine_Fan",
-            "6.2": "Distal_Submarine_Fan",
-            "6.3": "Marine_Turbidite",
-            "6.4": "Distal_Marine_Turbidites",
-            "6.5": "Marine_Deepwater"
-        }
-        return code_to_name[code]
-    except KeyError:
-        return None
-
+    return utils_func.removeDuplicate(depofacies)
 
 def handle_point(current_point, radius):
     current_point = int(current_point)
@@ -68,17 +19,6 @@ def handle_point(current_point, radius):
     return current_point
 
 
-def removeDuplicate(arr):
-    arr.sort()
-    i = 0
-    while i < len(arr) - 1:
-        if arr[i] == arr[i + 1]:
-            arr.pop(i)
-            i -= 1
-        i += 1
-    return arr
-
-
 with open("../initial_point/init_point.csv") as file:
     csv_reader = reader(file)
     headers = list(csv_reader)[0]
@@ -88,22 +28,22 @@ with open("../initial_point/init_point.csv") as file:
     data = list(dict_reader)
     for i in range(len(data)):
         if data[i]["Number_of_similar_units_50"] != "0":
-            unit_index = convert_string_to_array(data[i]["Index_of_similar_units_50"])
+            unit_index = utils_func.convert_string_to_array(data[i]["Index_of_similar_units_50"])
             idx = data[i]["Unit_index"]
             unit_index.append(idx)
             codes = find_unit_core_depofacies(unit_index, data)
             for code in codes:
-                name = map_core_depofacies_code_to_name(code)
+                name = utils_func.map_core_depofacies_code_to_name(code)
                 if name:
                     new_point = handle_point(data[i][name], "0-50")
                     print(new_point)
                     data[i].update({name: new_point})
 
         if data[i]["Number_of_similar_units_100"] != "0":
-            unit_index = convert_string_to_array(data[i]["Index_of_similar_units_100"])
+            unit_index = utils_func.convert_string_to_array(data[i]["Index_of_similar_units_100"])
             codes = find_unit_core_depofacies(unit_index, data)
             for code in codes:
-                name = map_core_depofacies_code_to_name(code)
+                name = utils_func.map_core_depofacies_code_to_name(code)
                 if name:
                     new_point = handle_point(data[i][name], "50-100")
                     print(new_point)
--- special_lithologies/main.py
+++ special_lithologies/main.py
@@ -1,84 +1,30 @@
 from csv import DictReader, DictWriter, reader
-
-groups = {
-    "Fluvial": ["1.1", "1.2", "1.3", "1.4", "1.5", "1.6"],
-    "Shallow_Lacustrine": ["2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7"],
-    "Deep_Lacustrine": ["3.1", "3.2", "3.3", "3.4", "3.5"],
-    "Marginal_Marine": ["4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7"],
-    "Shallow_Marine": ["5.1", "5.2", "5.3", "5.4", "5.5"],
-    "Deep_Marine": ["6.1", "6.2", "6.3", "6.4", "6.5"]
-}
-
-
-def map_core_depofacies_code_to_name(code):
-    try:
-        code_to_name = {
-            "1.1": "Alluvial_Fan",
-            "1.2": "Fluvial_Channel",
-            "1.3": "Fluvial_Point_Bar",
-            "1.4": "Levee",
-            "1.5": "Crevasse_Splay",
-            "1.6": "Fluvial_Floodplain",
-            "2.1": "Progradational_Lacustrine_Delta",
-            "2.2": "Lacustrine_Fan_Delta",
-            "2.3": "Progradational_Lacustrine_Shoreface",
-            "2.4": "Transgressive_Lacustrine_Shoreface",
-            "2.5": "Aggradational_Lacustrine_Shoreface",
-            "2.6": "Lacustrine_Offshore_Transition",
-            "2.7": "Lacustrine_Shelf",
-            "3.1": "Proximal_Sub-Lacustrine_Fan",
-            "3.2": "Distal_Sub-Lacustrine_Fan",
-            "3.3": "Lacustrine_Turbidite",
-            "3.4": "Distal_Lacustrine_Turbidites",
-            "3.5": "Lacustrine_Deepwater",
-            "4.1": "Marine_Delta",
-            "4.2": "Marine_Fan_Delta",
-            "4.3": "Tidal_Channel_and_Sand_Flat",
-            "4.4": "Sandy_Tidal_Flat",
-            "4.5": "Mixed_Tidal_Flat",
-            "4.6": "Muddy_Tidal_Flat",
-            "4.7": "Lagoon",
-            "5.1": "Progradational_Marine_Shoreface",
-            "5.2": "Transgressive_Marine_Shoreface",
-            "5.3": "Aggradational_Marine_Shoreface",
-            "5.4": "Marine_Offshore_Transition",
-            "5.5": "Marine_Shelf",
-            "6.1": "Proximal_Submarine_Fan",
-            "6.2": "Distal_Submarine_Fan",
-            "6.3": "Marine_Turbidite",
-            "6.4": "Distal_Marine_Turbidites",
-            "6.5": "Marine_Deepwater"
-        }
-        return code_to_name[code]
-    except KeyError:
-        return None
-
+from utilites import utils_func
 
 def add_point_volcanics(row):
     return row
 
-
 def add_point_marine_non_clastic(row):
     marines = ["Shallow_Marine", "Marginal_Marine", "Deep_Marine"]
     for group_name in marines:
-        for code in groups[group_name]:
-            name = map_core_depofacies_code_to_name(code)
+        codes = utils_func.get_group_depofacies(group_name)
+        for code in codes:
+            name = utils_func.map_core_depofacies_code_to_name(code)
             if int(row[name]) > 0:
                 row.update({name: int(row[name]) + 2})
     return row
 
 
 def add_point_coal(row):
-    for code in groups["Fluvial"]:
-        name = map_core_depofacies_code_to_name(code)
+    for code in utils_func.get_group_depofacies("Fluvial"):
+        name = utils_func.map_core_depofacies_code_to_name(code)
         if int(row[name]) > 0:
             row.update({name: int(row[name]) + 2})
 
-    for code in groups["Marginal_Marine"]:
-        name = map_core_depofacies_code_to_name(code)
+    for code in utils_func.get_group_depofacies("Marginal_Marine"):
+        name = utils_func.map_core_depofacies_code_to_name(code)
         if int(row[name]) > 0:
             row.update({name: int(row[name]) + 1})
-
     return row
 
 
@@ -101,24 +47,13 @@ def find_unit_special_lithology(unit_index, data):
     return lithologies
 
 
-def removeDuplicate(arr):
-    arr.sort()
-    i = 0
-    while i < len(arr) - 1:
-        if arr[i] == arr[i + 1]:
-            arr.pop(i)
-            i -= 1
-        i += 1
-    return arr
-
-
 def find_adjacent_unit_special_lithology(unit_index, data):
     before = find_unit_special_lithology(int(unit_index) - 1, data)
     current = find_unit_special_lithology(int(unit_index), data)
     after = find_unit_special_lithology(int(unit_index) + 1, data)
     before.extend(current)
     before.extend(after)
-    return removeDuplicate(before)
+    return utils_func.removeDuplicate(before)
 
 
 with open("../similar_unit/similar_unit.csv") as i_file:
